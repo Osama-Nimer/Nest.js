d9026262798da621e855ac4614cbc0b045b68fb7
Osama-Nimer <osamafares23@yahoo.com>
2025-09-18 18:09:23 +0300

    connection

--- src/configs/db.config.ts
+++ src/configs/db.config.ts
@@ -2,14 +2,11 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-      rejectUnauthorized: false, // allow self-signed certificates
-    },
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  keepAlive: true, 
 });
 
 export const db = drizzle(pool);
